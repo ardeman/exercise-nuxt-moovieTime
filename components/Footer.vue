dba30a83e2ed15cabf2d1fb945b59b05a6282358
Ardeman <ardeman@pintek.id>
2023-02-09 00:05:28 +0700

    Fix footer spacing

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,11 @@
 <template>
     <section class="bg-[rgba(0,0,0,0.29)] py-16">
-        <div class="flex justify-between items-center w-[1200px] m-auto text-[#929292] text-[13px]">
-            <span>&copy; 2023 MoovieTime. All rights reserved.</span>
-            <img src="/images/logo-grey.svg" alt="MoovieTime">
-            <span>Made with Nuxt 3</span>
+        <div class="grid grid-cols-3 items-center w-[1200px] m-auto text-[#929292] text-[13px]">
+            <span class="text-left">&copy; 2023 MoovieTime. All rights reserved.</span>
+            <div class="flex justify-center">
+                <img src="/images/logo-grey.svg" alt="MoovieTime">
+            </div>
+            <span class="text-right">Made with Nuxt 3</span>
         </div>
     </section>
 </template>
