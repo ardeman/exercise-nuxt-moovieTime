f078072c0a86dac8197c880478282a3996f9a671
Ardeman <ardeman@pintek.id>
2023-02-08 23:41:55 +0700

    Enhance poster image position

--- components/Movies/Detail.vue
+++ components/Movies/Detail.vue
@@ -1,12 +1,11 @@
 <template>
     <section class="z-0 mt-[-74px]">
-        <div class="relative h-[468px] flex flex-col bg-cover bg-center bg-opacity-20" :style="{ backgroundImage: 'linear-gradient(rgba(30, 35, 43, 0.8), rgba(30, 35, 43, 0.8)), url(https://image.tmdb.org/t/p/original' + details.backdrop_path + ')' }">
-            <div class="w-screen h-full absolute bottom-[-100px] z-20">
-                <div class="h-full flex items-end w-[1200px] mx-auto">
-                    <img :src="'https://image.tmdb.org/t/p/original' + details.poster_path" alt="Poster"
-                        class="w-[220px] drop-shadow-[0px_5px_10px_rgba(0,0,0,0.25)]">
-                </div>
-            </div>
+        <div 
+            class="relative h-[468px] flex flex-col bg-cover bg-center bg-opacity-20" 
+            :style="{ 
+                backgroundImage: 'linear-gradient(rgba(30, 35, 43, 0.8), rgba(30, 35, 43, 0.8)), url(https://image.tmdb.org/t/p/original' + details.backdrop_path + ')' 
+            }"
+        >
             <div class="w-screen h-full flex">
                 <div class="flex items-end w-[1200px] mx-auto">
                     <div class="pl-[253px] pb-8 w-full">
@@ -17,31 +16,35 @@
                 </div>
             </div>
             <div class="w-screen h-fit bg-[rgba(0,0,0,0.5)] flex-shrink-0">
-                <div class="flex gap-3 w-[1200px] m-auto pl-[253px] py-[20px]">
-                    <div class="flex gap-4 items-center flex-shrink-0">
-                        <img src="/images/star.svg" alt="Rating" class="w-[32px] h-[32px]">
-                        <p class="text-4xl font-semibold">{{ details.vote_average?.toFixed(1) }}</p>
-                    </div>
-                    <div class="flex text-xs items-center">
-                        <div class="mr-6 pr-6 border-r border-[rgba(255,255,255,0.2)] flex-shrink-0">
-                            <p class="text-[rgba(255,255,255,0.5)]">USER SCORE</p>
-                            <p>{{ details.vote_count }} VOTES</p>
+                <div class="w-[1200px] m-auto relative">
+                    <img :src="'https://image.tmdb.org/t/p/original' + details.poster_path" alt="Poster"
+                        class="w-[220px] drop-shadow-[0px_5px_10px_rgba(0,0,0,0.25)] absolute top-5 translate-y-[-50%] z-20">
+                    <div class="flex gap-3 pl-[253px] py-[20px]">
+                        <div class="flex gap-4 items-center flex-shrink-0">
+                            <img src="/images/star.svg" alt="Rating" class="w-[32px] h-[32px]">
+                            <p class="text-4xl font-semibold">{{ details.vote_average?.toFixed(1) }}</p>
                         </div>
-                        <div class="mr-6 pr-6 border-r border-[rgba(255,255,255,0.2)] flex-shrink-0">
-                            <p class="text-[rgba(255,255,255,0.5)]">STATUS</p>
-                            <p class="uppercase">{{ details.status }}</p>
-                        </div>
-                        <div class="mr-6 pr-6 border-r border-[rgba(255,255,255,0.2)] flex-shrink-0">
-                            <p class="text-[rgba(255,255,255,0.5)]">LANGUAGE</p>
-                            <p class="uppercase">{{ details.original_language }}</p>
-                        </div>
-                        <div class="mr-6 pr-6 border-r border-[rgba(255,255,255,0.2)] flex-shrink-0">
-                            <p class="text-[rgba(255,255,255,0.5)]">BUDGET</p>
-                            <p>{{ details?.budget?.toLocaleString('en-US', { style: 'currency', currency: 'USD' }) }}</p>
-                        </div>
-                        <div>
-                            <p class="text-[rgba(255,255,255,0.5)]">PRODUCTION</p>
-                            <p class="uppercase">{{ details.production_companies.map(company => company?.name)?.join(', ') }}</p>
+                        <div class="flex text-xs items-center">
+                            <div class="mr-6 pr-6 border-r border-[rgba(255,255,255,0.2)] flex-shrink-0">
+                                <p class="text-[rgba(255,255,255,0.5)]">USER SCORE</p>
+                                <p>{{ details.vote_count }} VOTES</p>
+                            </div>
+                            <div class="mr-6 pr-6 border-r border-[rgba(255,255,255,0.2)] flex-shrink-0">
+                                <p class="text-[rgba(255,255,255,0.5)]">STATUS</p>
+                                <p class="uppercase">{{ details.status }}</p>
+                            </div>
+                            <div class="mr-6 pr-6 border-r border-[rgba(255,255,255,0.2)] flex-shrink-0">
+                                <p class="text-[rgba(255,255,255,0.5)]">LANGUAGE</p>
+                                <p class="uppercase">{{ details.original_language }}</p>
+                            </div>
+                            <div class="mr-6 pr-6 border-r border-[rgba(255,255,255,0.2)] flex-shrink-0">
+                                <p class="text-[rgba(255,255,255,0.5)]">BUDGET</p>
+                                <p>{{ details?.budget?.toLocaleString('en-US', { style: 'currency', currency: 'USD' }) }}</p>
+                            </div>
+                            <div>
+                                <p class="text-[rgba(255,255,255,0.5)]">PRODUCTION</p>
+                                <p class="uppercase">{{ details.production_companies.map(company => company?.name)?.join(', ') }}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
