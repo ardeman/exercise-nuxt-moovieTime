c7096fa36b31183bd5276fcc31f2f4fbed804276
Ardeman <ardeman@pintek.id>
2023-02-26 16:27:00 +0700

    Prevent carousel title is more than 3 lines

--- components/Common/CarouselCard.vue
+++ components/Common/CarouselCard.vue
@@ -3,12 +3,12 @@
         :to="url"
         :class="'bg-black w-[520px] h-[328px] flex gap-6 flex-shrink-0 ' + classStatus"
     >
-        <img :src="posterUrl" alt="News World" class="my-[-20px] ml-[-20px]">
+        <img :src="posterUrl" :alt="title" class="my-[-20px] ml-[-20px]">
         <div class="pt-6 pr-9">
             <p class="flex gap-1.5 text-lg font-bold"><img src="/images/star.svg" alt="Rating" class="w-[16px]">
                 <span>{{ rate }}</span>
             </p>
-            <h3 class="text-3xl font-semibold">{{ title }}</h3>
+            <h3 class="text-3xl font-semibold max-h-[108px] overflow-hidden">{{ title }}</h3>
             <div class="flex gap-1.5 items-center py-3">
                 <span>{{ releaseYear }}</span>
                 <div class="w-[7px] h-[7px] rounded-full bg-[rgba(255,255,255,0.5)]"></div>
